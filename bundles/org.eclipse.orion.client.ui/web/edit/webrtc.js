3db7ce3fea6f85dd1c099e2e23067f76708b4602
ehdrms2034 <ehdrms2034@naver.com>
2018-09-17 20:37:30 +0900

    Web RTC부분
    WEB RTC부분 수정

---
+++ bundles/org.eclipse.orion.client.ui/web/edit/webrtc.js
@@ -0,0 +1,4 @@
+<script src="https://webrtc.github.io/adapter/adapter-latest.js"></script>
+<script src="https://cdn.webrtc-experiment.com/CodecsHandler.js"></script>
+<script src="https://cdn.webrtc-experiment.com/IceServersHandler.js"></script>
+<script src="https://cdn.webrtc-experiment.com/meeting.js"></script>
